7f523ff3f93465e028e732bd4d14985c957e6a5c
zsigmond.czine <zsigmond.czine@everit.biz>
2015-11-05 16:28:41 +0100

    update rescoure schema version and lqmg-maven-plugin version. Generate model

--- schema/src/main/generated/java/org/everit/authentication/simple/schema/qdsl/QSimpleSubject.java
+++ schema/src/main/generated/java/org/everit/authentication/simple/schema/qdsl/QSimpleSubject.java
@@ -15,15 +15,16 @@
  */
 package org.everit.authentication.simple.schema.qdsl;
 
-import static com.mysema.query.types.PathMetadataFactory.*;
+import static com.querydsl.core.types.PathMetadataFactory.*;
 
-import com.mysema.query.types.path.*;
+import com.querydsl.core.types.dsl.*;
 
-import com.mysema.query.types.PathMetadata;
+import com.querydsl.core.types.PathMetadata;
 import javax.annotation.Generated;
-import com.mysema.query.types.Path;
+import com.querydsl.core.types.Path;
 
-import com.mysema.query.sql.ColumnMetadata;
+import com.querydsl.sql.ColumnMetadata;
+import java.sql.Types;
 
 
 
@@ -31,8 +32,8 @@ import com.mysema.query.sql.ColumnMetadata;
 /**
  * QSimpleSubject is a Querydsl query type for QSimpleSubject
  */
-@Generated("com.mysema.query.sql.codegen.MetaDataSerializer")
-public class QSimpleSubject extends com.mysema.query.sql.RelationalPathBase<QSimpleSubject> {
+@Generated("com.querydsl.sql.codegen.MetaDataSerializer")
+public class QSimpleSubject extends com.querydsl.sql.RelationalPathBase<QSimpleSubject> {
 
     private static final long serialVersionUID = -719673281;
 
@@ -40,13 +41,7 @@ public class QSimpleSubject extends com.mysema.query.sql.RelationalPathBase<QSim
 
     public class PrimaryKeys {
 
-        public final com.mysema.query.sql.PrimaryKey<QSimpleSubject> authcSimpleSubjectPk = createPrimaryKey(simpleSubjectId);
-
-    }
-
-    public class ForeignKeys {
-
-        public final com.mysema.query.sql.ForeignKey<org.everit.resource.ri.schema.qdsl.QResource> simpleSubjectResourceFk = createForeignKey(resourceId, "resource_id");
+        public final com.querydsl.sql.PrimaryKey<QSimpleSubject> authcSimpleSubjectPk = createPrimaryKey(simpleSubjectId);
 
     }
 
@@ -59,8 +54,6 @@ public class QSimpleSubject extends com.mysema.query.sql.RelationalPathBase<QSim
     public final NumberPath<Long> simpleSubjectId = createNumber("simpleSubjectId", Long.class);
 
     public final PrimaryKeys pk = new PrimaryKeys();
-
-    public final ForeignKeys fk = new ForeignKeys();
 
     public QSimpleSubject(String variable) {
         super(QSimpleSubject.class, forVariable(variable), "org.everit.authentication.simple", "authc_simple_subject");
@@ -77,16 +70,16 @@ public class QSimpleSubject extends com.mysema.query.sql.RelationalPathBase<QSim
         addMetadata();
     }
 
-    public QSimpleSubject(PathMetadata<?> metadata) {
+    public QSimpleSubject(PathMetadata metadata) {
         super(QSimpleSubject.class, metadata, "org.everit.authentication.simple", "authc_simple_subject");
         addMetadata();
     }
 
     public void addMetadata() {
-        addMetadata(encryptedCredential, ColumnMetadata.named("encrypted_credential").ofType(12).withSize(256));
-        addMetadata(principal, ColumnMetadata.named("principal").ofType(12).withSize(256).notNull());
-        addMetadata(resourceId, ColumnMetadata.named("resource_id").ofType(-5).withSize(19).notNull());
-        addMetadata(simpleSubjectId, ColumnMetadata.named("simple_subject_id").ofType(-5).withSize(19).notNull());
+        addMetadata(encryptedCredential, ColumnMetadata.named("encrypted_credential").withIndex(3).ofType(Types.VARCHAR).withSize(256));
+        addMetadata(principal, ColumnMetadata.named("principal").withIndex(2).ofType(Types.VARCHAR).withSize(256).notNull());
+        addMetadata(resourceId, ColumnMetadata.named("resource_id").withIndex(4).ofType(Types.BIGINT).withSize(19).notNull());
+        addMetadata(simpleSubjectId, ColumnMetadata.named("simple_subject_id").withIndex(1).ofType(Types.BIGINT).withSize(19).notNull());
     }
 
 }
